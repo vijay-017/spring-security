05f89091477ae6501e7e791849c077a78db6d2f0
vijay <vijayasankarkambala008@gmail.com>
2026-05-24 23:24:10 +0530

    work with INMemoeyUserDetailsManager

---
+++ src/main/java/com/example/spring_security/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.example.spring_security;
+
+import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    @Bean
+    @Order(SecurityFilterProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+    
+    @Bean
+    public UserDetailsService UserDetailsService(){
+
+        UserDetails user1 = User.withUsername("vijay")
+                .password("{noop}vijay@123")
+                .roles("ADMIN").build();
+
+        UserDetails user2 = User.withUsername("sankar")
+                .password("{noop}sankar@123")
+                .roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+}
--- src/main/java/com/example/spring_security/controller/Controller.java
+++ src/main/java/com/example/spring_security/controller/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String Hello() {
 		return "Hello";
 	}
